7584c9eb85267fd9aa3fb0d0a370ec1c4a725a5b
Shivesh <shiveshummat@gmail.com>
2022-12-09 23:41:47 -0800

    a5 done

--- analysis.R
+++
@@ -1,4 +0,0 @@
-library(dplyr)
-library(shiny)
-
-data <- read.csv("../../data/owid-co2-data.csv")
--- shiny/app_server.R
+++ shiny/app_server.R
@@ -8,35 +8,6 @@ data <- read_csv("owid-co2-data.csv")
 # Only working with recent data
 data <- data %>% filter(year > 1950)
 
-# Markdown format for chart caption
-caption_md <- "Certain countries contribute more to emissions than others. 
-This interactive visualizations allows users to explore some of the most
-relevant countries trends in emissions and various other variables
-over the past 70 years. One interesting trend to note is that **China seems to have
-the steepest rate of increase of CO2 emissions** in recent years. Many other countries have 
-begun to slow their emissions. **UAE and US** have the highest CO2 emissions 
-per capita. This makes sense as these are **wealthier countries** that use more 
-fuel per person. China and Russia have the highest CO2 per GDP, indicating 
-they are highly **dependent on carbon emissions for their economy.**"
-
-# Markdown format for introduction
-intro_md <- paste0(
-"## Climate Change is one of the most significant issues facing the world today.
-
-To understand climate change in the world, we can analyze a dataset from _Our World in Data_. 
-This dataset was collected from various energy data sources and contains info on 
-different countries' emissions and other key metrics related to climate change over time. 
-
-In the world in 2021, ", formatC(total_co2, format = "d", big.mark = ","), " **million** tonnes of CO2 were emitted. Out of
-all continents, ", max_share_continent, " contributes the most to CO2 emissions, 
-making up ", max_share_amount, "% of emissions. One country also stands out as 
-that with the **most pollution per capita**, ", max_co2_capita, ". This country emitted ",
-max_co2_capita_amount, " tonnes per person of CO2 in 2021. Furthermore, ", max_energy_capita, 
-" also emits the most energy per capita. In 2021, this country emitted ", formatC(max_energy_capita_amount, format = "d", big.mark = ","), 
-" kilowatt-hours of energy per person. It is important to keep in mind the emissions 
-per person as well as total, as larger countries may be unnecessarily targeted when looking at totals.
-Hopefully, climate change is tackled where emissions are the most unnecessary, and we 
-can move towards **a more sustainable planet.**")
 
 # Calculating summary statistics
 max_co2_capita <- data %>% filter(year == 2021) %>% 
@@ -72,6 +43,35 @@ max_share_amount <- round(data %>% filter(year == 2021) %>%
   filter(share_global_cumulative_co2 == max(share_global_cumulative_co2, na.rm = T)) %>% 
   pull(share_global_cumulative_co2))
 
+# Markdown format for chart caption
+caption_md <- "Certain countries contribute more to emissions than others. 
+This interactive visualizations allows users to explore some of the most
+relevant countries trends in emissions and various other variables
+over the past 70 years. One interesting trend to note is that **China seems to have
+the steepest rate of increase of CO2 emissions** in recent years. Many other countries have 
+begun to slow their emissions. **UAE and US** have the highest CO2 emissions 
+per capita. This makes sense as these are **wealthier countries** that use more 
+fuel per person. China and Russia have the highest CO2 per GDP, indicating 
+they are highly **dependent on carbon emissions for their economy.**"
+
+# Markdown format for introduction
+intro_md <- paste0(
+"## Climate Change is one of the most significant issues facing the world today.
+
+To understand climate change in the world, we can analyze a dataset from _Our World in Data_. 
+This dataset was collected from various energy data sources and contains info on 
+different countries' emissions and other key metrics related to climate change over time. 
+
+In the world in 2021, ", formatC(total_co2, format = "d", big.mark = ","), " **million** tonnes of CO2 were emitted. Out of
+all continents, ", max_share_continent, " contributes the most to CO2 emissions, 
+making up ", max_share_amount, "% of emissions. One country also stands out as 
+that with the **most pollution per capita**, ", max_co2_capita, ". This country emitted ",
+max_co2_capita_amount, " tonnes per person of CO2 in 2021. Furthermore, ", max_energy_capita, 
+" also emits the most energy per capita. In 2021, this country emitted ", formatC(max_energy_capita_amount, format = "d", big.mark = ","), 
+" kilowatt-hours of energy per person. It is important to keep in mind the emissions 
+per person as well as total, as larger countries may be unnecessarily targeted when looking at totals.
+Hopefully, climate change is tackled where emissions are the most unnecessary, and we 
+can move towards **a more sustainable planet.**")
 
 # Server function for chart and markdown rendering
 server <- function(input, output) {
